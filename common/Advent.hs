48211ecb7ad1cb7995ba2b311da0ff4e9fd7401c
Paolo Martini <mrtnpaolo@protonmail.com>
2023-12-02 15:10:16 +0100

    🐣 advent of code 2023

---
+++ common/Advent.hs
@@ -0,0 +1,13 @@
+module Advent
+  ( module Advent.Coord
+  , module Advent.Input
+  , module Advent.Search
+  , module Advent.Chinese
+  , module Advent.List
+  ) where
+
+import Advent.Coord
+import Advent.Input
+import Advent.Search
+import Advent.Chinese
+import Advent.List
