8b18831477b5987db50c781966191911c1bfc880
Paolo Martini <mrtnpaolo@protonmail.com>
2023-12-07 21:00:01 +0100

    🔪 day-07: compute the best hand without classifying alternatives

--- execs/Day07.hs
+++ execs/Day07.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import Advent     (getInputLines)
-import Data.List  (sortOn,sort,group,elemIndex,nub)
+import Data.List  (sortOn,sort,group,elemIndex)
 import Data.Maybe (mapMaybe)
 
 main =
@@ -15,14 +15,14 @@ winnings hands = sum [ rank*bid | rank <- [1..] | (_,bid) <- hands ]
 
 part1 = winnings . sortOn (\(hand,_) -> classify hand : value "23456789TJQKA" hand)
 
+value cards = mapMaybe (`elemIndex` cards)
+
 classify (sort . map length . group . sort -> typ) = n
   where
     Just n = typ `elemIndex` [[1,1,1,1,1],[1,1,1,2],[1,2,2],[1,1,3],[2,3],[1,4],[5]]
 
-value cards = mapMaybe (`elemIndex` cards)
+part2 = winnings . sortOn (\(hand,_) -> classify (maximize hand) : value "J23456789TQKA" hand)
 
-part2 = winnings . sortOn (\(hand,_) -> jokers hand : value "J23456789TQKA" hand)
-
-jokers hand = maximum [ classify (map replace hand)
-                      | card <- nub hand
-                      , let replace = \case 'J' -> card; x -> x ]
+maximize hand = map (\case 'J' -> best; c -> c) hand
+  where
+    (best:_) = (filter ('J' /=) . map head . sortOn (negate . length) . group . sort $ hand) ++ "J"
