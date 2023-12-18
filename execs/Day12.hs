cf59db4f72f98ce8394a445ae6dcca0bcb056b24
Paolo Martini <mrtnpaolo@protonmail.com>
2023-12-18 14:32:37 +0100

    🧼 day-12: clean up

--- execs/Day12.hs
+++ execs/Day12.hs
@@ -1,109 +1,34 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split          qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent          (getInputLines,memo2)
+import Data.List       (intercalate)
+import Data.List.Split (splitOn)
 
 main =
   do inp <- getInputLines parse 12
      print (part1 inp)
      print (part2 inp)
   where
-    parse (words -> [xs,(map (read @Int) . L.splitOn "," -> ns)]) = (xs,ns)
+    parse (words -> [xs,(map (read @Int) . splitOn "," -> ns)]) = (xs,ns)
 
-unfold = concat . L.intersperse "?" . replicate 5
+part1 = sum . map (uncurry count)
 
-part1 inp = sum (map f inp)
-
-part2 = part1 . map (\(unfold -> xs,concat . replicate 5 -> ns) -> (xs,ns))
-
-f (xs,ns) = go xs ns
+part2 = part1 . map unfold
   where
-    go = memo2 \cases
-      []         [] -> 1
-      []         _  -> 0
-      xs@('.':_) ns -> go (dropWhile ('.'==) xs) ns
-      ('?':rest) ns -> go ('.':rest) ns + go ('#':rest) ns
-      ('#':_   ) [] -> 0
-      xs@('#':_) (n:ns)
-        | length xs < n           -> 0
-        | ('.'==) `any` take n xs -> 0
-        | not (null ns) ->
-          case drop n xs of
-            []    -> 0
-            '#':_ -> 0
-            _     -> go (drop (n+1) xs) ns
-        | otherwise -> go (drop n xs) ns
+    unfold (xs,ns) = (intercalate "?" (replicate 5 xs),concat (replicate 5 ns))
 
-{-
-f (unfold -> xs,concat . replicate 5 -> ns) = (xs,ys)
-  where
-    ys = L.splitOn "." xs
--}
-
-{-
-unfold c = concat . L.intersperse c . replicate 5
-
-part2 = map f
-
-f (unfold "?" -> xs,concat . replicate 5 -> ns) = count ((ns ==) . h) (g ns xs)
-
-g [] _  = []
-g _  [] = [[]]
-g ns@(m:ms) (x:xs) =
-  case x of
-    '.' -> map ('.':) (g ns xs)
-    '#' -> map ('#':) (g ((m-1):ms) xs)
-    '?' -> map ('#':) (g ((m-1):ms) xs) ++ map ('.':) (g ns xs)
-
-h = map length . filter (not . null) . L.splitOn "."
--}
-
-{-
-f (xs,ns) = length [ ys | ys <- g xs, h ys == ns ]
-
-g [] = [[]]
-g ('?':xs) = map ('#':) ys ++ map ('.':) ys where ys = g xs
-g (x:xs) = map (x:) (g xs)
-
-h = map length . filter (not . null) . L.splitOn "."
-
-unfold (xs,ns) = (ys,ns)
-  where
-    ys = (concat . L.intersperse "?" . replicate 5) xs
-
-part2 = sum . map (f . unfold)
--}
-
-{-
-main =
-  do inp <- getInputLines parse 12
-     print (part1 inp)
-     print (part2 inp)
-  where
-    parse (words -> [xs,(map (read @Int) . L.splitOn "," -> ns)]) = (xs,ns)
-
-part1 = sum . map f
-
-f (xs,ns) = length [ ys | ys <- g xs, h ys == ns ]
-
-g [] = [[]]
-g ('?':xs) = map ('#':) ys ++ map ('.':) ys where ys = g xs
-g (x:xs) = map (x:) (g xs)
-
-h = map length . filter (not . null) . L.splitOn "."
-
-part2 = const ()
--}
+count = memo2 \cases
+  []         [] -> 1
+  []         _  -> 0
+  xs@('.':_) ns -> count (dropWhile ('.'==) xs) ns
+  ('?':rest) ns -> count ('.':rest) ns + count ('#':rest) ns
+  ('#':_   ) [] -> 0
+  xs@('#':_) (n:ns)
+    | length xs < n           -> 0
+    | ('.'==) `any` take n xs -> 0
+    | not (null ns) ->
+      case drop n xs of
+        []    -> 0
+        '#':_ -> 0
+        _     -> count (drop (n+1) xs) ns
+    | otherwise -> count (drop n xs) ns
