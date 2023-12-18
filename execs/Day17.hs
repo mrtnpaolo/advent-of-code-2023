11e65e5dac943394057b849730f3edbb09d3ad25
Paolo Martini <mrtnpaolo@protonmail.com>
2023-12-18 14:30:46 +0100

    🧼 day-17: clean up

--- execs/Day17.hs
+++ execs/Day17.hs
@@ -1,52 +1,26 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split    qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent             (getInputArray)
+import Advent.Coord       (Coord(..),origin,east,south,turnLeft,turnRight,addCoord)
+import Advent.Search      (AStep(..),astarN)
+import Data.Char          (digitToInt)
+import Data.Array.Unboxed (amap,bounds,(!))
 
 main =
-  do inp <- A.amap digitToInt <$> getInputArray 17
-     let (cm,cM) = A.bounds inp
-     print ("dest",cM)
-{-
-     let (a,b,xs) = part1 cM inp
-     print a
-     print b
-     putStrLn $ showCoordsWith 1 pixel xs
--}
-     print (part1 cM inp 1 3)
-     print (part1 cM inp 4 10)
-
-{-
-pixel N = "^"
-pixel E = ">"
-pixel S = "v"
-pixel W = "<"
-
-fst3 (a,_,_) = a
-snd3 (_,b,_) = b
-trd3 (_,_,c) = c
--}
+  do inp <- amap digitToInt <$> getInputArray 17
+     let (_,cM) = bounds inp
+     print (part1 cM inp)
+     print (part2 cM inp)
 
 data St = St !Coord !Coord !Int -- position, dir, distance
-  deriving (Eq,Ord,Show)
+  deriving (Eq,Ord)
 
--- astarN ::  -> [(St,Int)
-part1 dest@(C yd xd) a distm distM = [best]
+part1 = search 1 3
+part2 = search 4 10
+
+search distm distM dest@(C yd xd) a = best
   where
-    best = head $ [ cost | (St c _ _,cost) <- astarN nexts starts, c == dest ]
+    best = head [ cost | (St c _ _,cost) <- astarN nexts starts, c == dest ]
 
     starts :: [St]
     starts = [ St origin east 0, St origin south 0 ]
@@ -54,82 +28,13 @@ part1 dest@(C yd xd) a distm distM = [best]
     nexts :: St -> [AStep St]
     nexts (St c dir dist) =
       [ AStep { astepNext = St c' dir' dist'
-              , astepCost = a A.! c'
+              , astepCost = a ! c'
               , astepHeuristic = 0 }
       | (dir',dist') <-
-        [ (dir,dist+1) | dist < distM ] ++
+        [ (dir,dist+1)      | dist <  distM ] ++
         [ (turnLeft  dir,1) | dist >= distm ] ++
         [ (turnRight dir,1) | dist >= distm ]
       , let c' = c `addCoord` dir'
-      , inside c'
-      ]
+      , inside c' ]
 
     inside (C y x) = 0 <= y && y <= yd && 0 <= x && x <= xd
-
-{-
-part1 dest@(C yd xd) a = take 10 ends
-  where
-    -- best = head $ L.sortOn (snd . fst3) $ take 100 ends
-
-    ends = [ (c,cost) | (((c,_),_,_),cost) <- travel, c==C 1 5 ]
-
-    -- astarOn :: Ord r => (a -> r) -> (a -> [(a,Int,Int)]) -> a -> [(a,Int)]
-    travel = astarOn repr nexts start
-
-    start = ( (origin,0), (S,2), [(origin,S)] )
-
-    heat = (a A.!)
-
-    repr = fst3
-
-    nexts ((c,h),d,xs)
-      | c == dest = []
-      | otherwise = [ ( ((c',h + heat c'),d',xs++[(c',dir)])
-                      , heat c'
-                      , 0 )
-                    | d'@(dir,_) <- new d, let c' = c `addCoord` fst d', inside c' ]
-
-    inside (C y x) = 0 <= y && y <= yd && 0 <= x && x <= xd
--}
-
-{-
--- too high: 1553 (1554 minus origin)
-part1 dest@(C yd xd) a = best
-  where
-    best = head $ L.sortOn (snd . fst3) $ take 100 ends
-
-    ends = filter ((dest ==) . fst . fst3) travel
-
-    travel = bfsOn repr nexts starts
-
-    starts =
-      [ ( (right origin,heat (right origin)) , (E,1), [(right origin,E)] )
-      , ( (below origin,heat (below origin)) , (S,1), [(below origin,S)] ) ]
-
-    heat = (a A.!)
-
-    repr = fst3
-
-    nexts ((c,h),d,xs)
-      | c == dest = []
-      | otherwise = [ ((c',h + heat c'),d',xs++[(c',dir)])
-                    | d'@(dir,_) <- new d, let c' = c `addCoord` fst d', inside c' ]
-
-    inside (C y x) = 0 <= y && y <= yd && 0 <= x && x <= xd
--}
-
-{-
-new (E,3) = [(N,1),(S,1)]
-new (E,n) = (E,n+1) : new (E,3)
-
-new (W,3) = [(S,1),(N,1)]
-new (W,n) = (W,n+1) : new (W,3)
-
-new (N,3) = [(W,1),(E,1)]
-new (N,n) = (N,n+1) : new (N,3)
-
-new (S,3) = [(E,1),(W,1)]
-new (S,n) = (S,n+1) : new (S,3)
--}
-
-part2 = const ()
