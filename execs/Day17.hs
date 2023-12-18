b37d8cb393ecffb2e347fc5a9b8a893a2e2c6ce4
Paolo Martini <mrtnpaolo@protonmail.com>
2023-12-18 13:57:48 +0100

    🎄 day-17: part 2

--- execs/Day17.hs
+++ execs/Day17.hs
@@ -26,8 +26,8 @@ main =
      print b
      putStrLn $ showCoordsWith 1 pixel xs
 -}
-     mapM_ print (part1 cM inp)
-     print (part2 inp)
+     print (part1 cM inp 1 3)
+     print (part1 cM inp 4 10)
 
 {-
 pixel N = "^"
@@ -44,7 +44,7 @@ data St = St !Coord !Coord !Int -- position, dir, distance
   deriving (Eq,Ord,Show)
 
 -- astarN ::  -> [(St,Int)
-part1 dest@(C yd xd) a = [best]
+part1 dest@(C yd xd) a distm distM = [best]
   where
     best = head $ [ cost | (St c _ _,cost) <- astarN nexts starts, c == dest ]
 
@@ -57,9 +57,9 @@ part1 dest@(C yd xd) a = [best]
               , astepCost = a A.! c'
               , astepHeuristic = 0 }
       | (dir',dist') <-
-        [ (dir,dist+1) | dist < 3 ] ++
-        [ (turnLeft  dir,1) ] ++
-        [ (turnRight dir,1) ]
+        [ (dir,dist+1) | dist < distM ] ++
+        [ (turnLeft  dir,1) | dist >= distm ] ++
+        [ (turnRight dir,1) | dist >= distm ]
       , let c' = c `addCoord` dir'
       , inside c'
       ]
