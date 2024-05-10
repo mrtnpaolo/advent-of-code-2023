107ca9b8ae502cce560921160962a49d74e32c90
Paolo Martini <mrtnpaolo@protonmail.com>
2024-05-11 00:27:20 +0200

    🎄 day-18: part 2

--- execs/Day18.hs
+++ execs/Day18.hs
@@ -17,28 +17,31 @@ import Data.Array.Unboxed qualified as A
 import Debug.Trace
 
 main =
-  do inp <- getInputLines parse 18
-     let (_,s) = dig inp
-     putStrLn (drawCoords (M.fromList [(c,'#')|c<-S.toList s]))
-     print (C 0 0 `S.member` s)
-     print (C 0 1 `S.member` s)
-     print (C 1 0 `S.member` s)
-     print (part1 s)
+  do inp <- getInputLines parse1 18
+     print (part1 inp)
+     inp <- getInputLines parse2 18
+     print (part2 inp)
   where
-    parse x = let [d:_,read @Int -> n,_] = f x in (d,n)
-    f = words . map \case c | c `elem` "(#)" -> ' '; c -> c
+    parse1 (words -> [[dir -> c],read @Int -> n,_]) = scaleCoord n c
 
-dig = L.foldl' go (origin,S.singleton origin)
-  where
-    go (c,s) (d,n) = (c',s')
+    dir 'U' = north; dir 'D' = south; dir 'L' = west; dir 'R' = east
+
+    parse2 = decode . last . words . filter (`notElem` "(#)")
+
+    decode xs = scaleCoord n c
       where
-        new = take n $ tail $ iterate (move d) c
-        c' = last new
-        s' = S.fromList new `S.union` s
+        c = dir ("RDLU" !! digitToInt (last xs))
+        [(n,_)] = readHex (init xs)
 
-move 'U' = above; move 'R' = right; move 'D' = below; move 'L' = left
+part1 = area . polygon
 
-part1 s = S.size s + length interior
-  where
-    interior = bfs inside (C 1 1)
-    inside c = [ c' | c' <- neighbors c, c' `S.notMember` s ]
+part2 = part1
+
+polygon = L.scanl' addCoord origin
+
+area p = shoelace p + perimeter p `div` 2 + 1
+
+perimeter p = sum [ abs (y'-y) + abs (x'-x) | (C y x,C y' x') <- zip p (tail p) ]
+
+shoelace [_] = 0
+shoelace (C y x : xs@(C y' x' : _)) = (y + y') * (x - x') `div` 2 + shoelace xs
