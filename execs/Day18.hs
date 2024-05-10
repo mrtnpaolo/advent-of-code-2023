b501d7e809c7e8b81151a15b398b87684aa070ec
Paolo Martini <mrtnpaolo@protonmail.com>
2024-05-11 00:33:25 +0200

    🧼 day-18: clean up

--- execs/Day18.hs
+++ execs/Day18.hs
@@ -1,20 +1,9 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split    qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent.Input (getInputLines)
+import Advent.Coord (Coord(..),origin,north,south,west,east,scaleCoord,addCoord)
+import Numeric      (readHex)
+import Data.List    (scanl')
 
 main =
   do inp <- getInputLines parse1 18
@@ -23,25 +12,18 @@ main =
      print (part2 inp)
   where
     parse1 (words -> [[dir -> c],read @Int -> n,_]) = scaleCoord n c
-
     dir 'U' = north; dir 'D' = south; dir 'L' = west; dir 'R' = east
-
     parse2 = decode . last . words . filter (`notElem` "(#)")
-
-    decode xs = scaleCoord n c
-      where
-        c = dir ("RDLU" !! digitToInt (last xs))
-        [(n,_)] = readHex (init xs)
+    decode (readHex -> [((`quotRem` 16) -> (n,r),_)]) = scaleCoord n $ dir ("RDLU" !! r)
 
 part1 = area . polygon
 
 part2 = part1
 
-polygon = L.scanl' addCoord origin
+polygon = scanl' addCoord origin
 
-area p = shoelace p + perimeter p `div` 2 + 1
+area p = abs (shoelace p) + perimeter p `div` 2 + 1
 
-perimeter p = sum [ abs (y'-y) + abs (x'-x) | (C y x,C y' x') <- zip p (tail p) ]
+shoelace p = sum [ (y + y')*(x - x') | C y x <- p | C y' x' <- tail p ] `div` 2
 
-shoelace [_] = 0
-shoelace (C y x : xs@(C y' x' : _)) = (y + y') * (x - x') `div` 2 + shoelace xs
+perimeter p = sum [ abs (y'-y) + abs (x'-x) | C y x <- p | C y' x' <- tail p ]
